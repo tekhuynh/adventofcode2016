f2b5f5c550bc878b9b0874d798e965feb822294e
Tek Huynh <tekhuynh@gmail.com>
2016-12-03 15:16:23 +1100

    day 2 update

--- 2-2.pl
+++ 2-2.pl
@@ -8,44 +8,24 @@
   [0,0,'D',0,0],
 );
 
-@directions = (
-  [ 0,  1],
-  [ 1,  0],
-  [ 0, -1],
-  [-1,  0]
+%directions = (
+  'R' => [ 0,  1],
+  'D' => [ 1,  0],
+  'L' => [ 0, -1],
+  'U' => [-1,  0]
 );
 
 @pos = (2, 0);
 @ans = ();
 
-while ($line = <>) {
-  chomp($line);
+while (chomp($line = <>)) {
   @moves = split '', $line;
   foreach $m (@moves) {
 
     @position = @pos;
 
-    if ($m eq 'U') {
-
-      $position[0] += $directions[3][0];
-      $position[1] += $directions[3][1];
-
-    } elsif ($m eq 'D') {
-
-      $position[0] += $directions[1][0];
-      $position[1] += $directions[1][1];
-
-    } elsif ($m eq 'L') {
-
-      $position[0] += $directions[2][0];
-      $position[1] += $directions[2][1];
-
-    } elsif ($m eq 'R') {
-
-      $position[0] += $directions[0][0];
-      $position[1] += $directions[0][1];
-
-    }
+    $position[0] += $directions{$m}[0];
+    $position[1] += $directions{$m}[1];
 
     $position[0] = 0 if $position[0] < 0;
     $position[1] = 0 if $position[1] < 0;
